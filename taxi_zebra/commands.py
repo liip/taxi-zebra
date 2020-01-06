8d367d48391a03e100ccd5e5e7e6cee5fdee35df
Stefan Heinemann <stefan.heinemann@codedump.ch>
2020-01-06 15:50:37 +0100

    Display the hours done today in the balance output

--- taxi_zebra/commands.py
+++ taxi_zebra/commands.py
@@ -64,9 +64,12 @@ def balance(ctx):
     hours_to_be_pushed = timesheet_collection.get_hours(pushed=False, ignored=False, unmapped=False)
 
     today = datetime.date.today()
+
     user_info = backend.get_user_info()
     timesheets = backend.get_timesheets(get_first_dow(today), get_last_dow(today))
+    timesheets_today = backend.get_timesheets(today, today)
     total_duration = sum([float(timesheet['time']) for timesheet in timesheets])
+    total_duration_today = sum([float(timesheet['time']) for timesheet in timesheets_today])
 
     vacation = hours_to_days(user_info['vacation']['difference'])
     vacation_balance = '{} days, {:.2f} hours'.format(*vacation)
@@ -76,4 +79,5 @@ def balance(ctx):
     click.echo("Hours balance: {}".format(signed_number(hours_balance)))
     click.echo("Hours balance after push: {}".format(signed_number(hours_balance + hours_to_be_pushed)))
     click.echo("Hours done this week: {:.2f}".format(total_duration))
+    click.echo("Hours done today: {:.2f}".format(total_duration_today + hours_to_be_pushed))
     click.echo("Vacation left: {}".format(vacation_balance))
