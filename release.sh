b94409625eadbea586cc7fe9ff40841731c690d9
Jocelyn Thode <jocelyn@thode.email>
2025-07-08 08:10:06 +0200

    Fix script

--- release.sh
+++ release.sh
@@ -1,27 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 release() {
-	set -e
-	echo "__version__ = '$1'" >taxi_zebra/__init__.py
-	git commit -m "Bump version number to $1" taxi_zebra/__init__.py
-	git tag -m "Release $1" $1
-	git push
-	git push origin $1
+  set -e
+  echo "__version__ = \"$1\"" >taxi_zebra/__init__.py
+  git commit -m "Bump version number to $1" taxi_zebra/__init__.py
+  git tag -m "Release $1" "$1"
+  git push
+  git push origin "$1"
 }
 
 if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 VERSION" >&2
-	exit 1
+  echo "Usage: $0 VERSION" >&2
+  exit 1
 fi
 
 while true; do
-	read -p "Are you sure you want to release version '$1'? [y/n] " yn
-	case $yn in
-	[Yy]*)
-		release $1
-		break
-		;;
-	[Nn]*) exit ;;
-	*) echo "Please answer y or n." ;;
-	esac
+  read -rp "Are you sure you want to release version '$1'? [y/n] " yn
+  case $yn in
+    [Yy]*)
+      release "$1"
+      break
+      ;;
+    [Nn]*) exit ;;
+    *) echo "Please answer y or n." ;;
+  esac
 done
